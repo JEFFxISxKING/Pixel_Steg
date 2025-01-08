c98d3944c46278a82f5aa79cbf908344bd046732
Jeff TheLandshark <JeffxIsxKing@gmail.com>
2025-01-08 10:30:04 -0600

    Update Convert to Grey.py

--- Convert to Grey.py
+++ Convert to Grey.py
@@ -1,15 +1,12 @@
 from PIL import Image
-import numpy as np
 
 def save_image(img, output_path):
     """Save the image to a specified path."""
     img.save(output_path)
     print(f"Image saved to {output_path}")
 
+image_path = 'JEFF_01.png' #Get input image
 
-image_path = 'JEFF_01.png'
-
-img = Image.open(image_path)
-img = img.convert('L')
-img = img.convert('RGB')
-save_image(img, 'New.png')
+img = Image.open(image_path) #open image
+img = img.convert('L') #convert to grey ("RGB" gives RGB)
+save_image(img, 'New.png') #Save image
